d48fe48ce586cf83ecb5fa999c27a279dbb07398
Adam Curtis <kallisti.dev@gmail.com>
2023-04-29 12:57:45 -0400

    parse a syn::File instead of syn::ItemMod for top-level macro invocation

--- salt-macros/src/lib.rs
+++ salt-macros/src/lib.rs
@@ -1,17 +1,6 @@
 use proc_macro2::Span;
 use quote::{quote_spanned, ToTokens};
-use syn::{ItemFn, ItemMod};
-
-struct SaltBin {
-    main: SaltMainFn,
-    modules: Vec<SaltModule>,
-}
-
-impl ToTokens for SaltBin {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        todo!()
-    }
-}
+use syn::{Attribute, Item, ItemFn, ItemMod};
 
 struct SaltMainFn;
 
@@ -33,16 +22,16 @@ pub fn main(
     attr: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    if let Ok(input) = syn::parse::<ItemMod>(input.clone()) {
+    if let Ok(input) = syn::parse::<syn::File>(input.clone()) {
         parse_salt_bin(input).to_token_stream().into()
     } else if let Ok(input) = syn::parse::<ItemFn>(input) {
         parse_salt_main(input).to_token_stream().into()
     } else {
-        quote_spanned! { Span::call_site() => compile_error!("Invalid salt::main definition: must be a module or main funciton") }.into()
+        quote_spanned! { Span::call_site() => compile_error!("Invalid salt::main invocation: must be attached to source file or main funciton") }.into()
     }
 }
 
-fn parse_salt_bin(module: ItemMod) -> SaltBin {
+fn parse_salt_bin(bin_file: syn::File) -> syn::File {
     todo!()
 }
 
